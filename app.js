9ae3c7fd9471894fe425b508f091a76564981760
tamara97.b <tamarabassam1997@icloud.com>
2021-07-05 14:40:24 +0300

    [poiuy

--- app.js
+++ app.js
@@ -1,95 +1,113 @@
-'use strict'
-let arr =[];
-function Pic (name,path,time){
+Pic.arr = [];
+
+new Pic('bag', 'images/bag.jpg');
+new Pic('banana', 'images/banana.jpg');
+new Pic('bathroom', 'images/bathroom.jpg');
+new Pic('boots', 'images/boots.jpg');
+new Pic('breakfast', 'images/breakfast.jpg');
+new Pic('bubblegum', 'images/bubblegum.jpg');
+new Pic('chair', 'images/chair.jpg');
+new Pic('cthulhu', 'images/cthulhu.jpg');
+new Pic('dog-duck', 'images/dog-duck.jpg');
+new Pic('dragon', 'images/dragon.jpg');
+new Pic('pen', 'images/pen.jpg');
+new Pic('pet-sweep', 'images/pet-sweep.jpg');
+new Pic('scissors', 'images/scissors.jpg');
+new Pic('shark', 'images/shark.jpg');
+new Pic('sweep', 'images/sweep.png');
+new Pic('tauntaun', 'images/tauntaun.jpg');
+new Pic('unicorn', 'images/unicorn.jpg');
+new Pic('water-can', 'images/water-can.jpg');
+new Pic('wine-glass', 'images/wine-glass.jpg');
+
+
+let section = document.getElementById('section');
+let firstImage = document.getElementById('firstImage');
+let secondImage = document.getElementById('secondImage');
+let thirdImage = document.getElementById('thirdImage');
+
+const max = 25;
+let counter = 0;
+
+
+function Pic(name, path) {
     this.name = name;
     this.path = path;
-    this.time =time;
-    arr.push(this);
+    this.votes = 0;
+    this.shown = 0;
+    Pic.arr.push(this);
 }
-new Pic ('bag','images/bubblegum.jpg');
-new Pic ('banana','images/banana.jpg');
-new Pic ('bathroom','images/bathroom.jpg');
-new Pic ('boots','images/boots.jpg');
-new Pic ('breacfast','images/breakfast.jpg');
-new Pic ('bubblegum','images/bubblegum.jpg');
-new Pic ('chair','images/chair.jpg');
-new Pic ('cthulhu','images/cthulhu.jpg');
-new Pic ('dog-duck','images/dog-duck.jpg');
-new Pic ('dragon','images/dragon.jpg');
-new Pic ('pen','images/pen.jpg');
-new Pic ('pet-sweep','images/pet-sweep.jpg');
-new Pic ('scissors','images/scissors.jpg');
-new Pic ('shark','images/shark.jpg');
-new Pic ('sweep','images/sweep.png');
-new Pic ('tauntaun','images/tauntaun.jpg');
-new Pic ('unicorn','images/unicorn.jpg');
-new Pic ('water-can','images/water-can.jpg');
-new Pic ('wine-glass','images/wine-glass.jpg');
 
-let max =25;
-let count= 0;
+let first;
+let second;
+let third;
 
-// console.log(arr);
-function randomnum (){
-    return Math.floor(Math.random()*arr.length)
+function getImages() {
+    first = generateRandom();
+    second = generateRandom();
+    third = generateRandom();
+
+    while (first === second || first === third || second === third) {
+        first = generateRandom();
+        third = generateRandom();
+    }
+
+
+    firstImage.src = Pic.arr[first].path;
+    secondImage.src = Pic.arr[second].path;
+    thirdImage.src = Pic.arr[third].path;
+    Pic.arr[first].shown++;
+    Pic.arr[second].shown++;
+    Pic.arr[third].shown++;
+
 }
-console.log(randomnum())
+getImages();
 
-let first = document.getElementById('firstImage')
-let second=document.getElementById('secondImage')
-let third=document.getElementById('thirdImage')
 
-let firstNum
-        let secondNum
-        let thirdNum
+section.addEventListener('click', clicking);
 
-function tamara (){
-    firstNum=randomnum();
-    secondNum=randomnum();
-     thirdNum=randomnum();
-    while (firstNum===secondNum|| secondNum===thirdNum||firstNum===thirdNum) {
-        firstNum=randomnum();
-        secondNum=randomnum();
-         thirdNum=randomnum();
-        
+function clicking(event) {
+    counter++;
+
+    if (max >= counter) {
+        if (event.target.id === 'firstImage') {
+            Pic.arr[first].votes++;
+
+        } else if (event.target.id === 'secondImage') {
+            Pic.arr[second].votes++;
+
+        } else if (event.target.id === 'thirdImage') {
+            Pic.arr[third].votes++;
+        }
+        getImages();
+
+    } else {
+        let btn = document.getElementById('btn');
+        btn.addEventListener('click', list);
+
+    }
+    
+}
+
+
+function list() {
+    let ul = document.getElementById('unList');
+    for (let i = 0; i < Pic.arr.length; i++) {
+        let listItem = document.createElement('li');
+        ul.appendChild(listItem);
+        listItem.textContent = `${Pic.arr[i].name} ${Pic.arr[i].votes} votes , seen ${Pic.arr[i].shown}`;
         
     }
-    first.src=arr[firstNum].path
-    second.src=arr[secondNum].path
-    third.src=arr[thirdNum].path
-    
-    
-}
-tamara()
+    btn.removeEventListener('click', list);
+    section.removeEventListener('click', clicking);
 
-first.addEventListener('click',clicking)
-second.addEventListener('click',clicking)
-third.addEventListener('click',clicking)
-function clicking (){
-    count++
-    // console.log(count);
-//     if (count<=max){
-// if(event.target.id==='firstImage'){
-//     Pic.arr[firstNum].vote++;
-    
-// }
-// else if(event.target.id==='secondImage'){
-// Pic.arr[secondNum].vote++;
-//     }
-//  else if (event.target.id==='thirdImage') {
-//     Pic.arr[thirdNum].vote++;
-//  }
-// tamara();
-// }else {
-//     List();
-// }
-// }
-// function List(){
-//     const unorder =document.getElementById('unorder');
-//     for(let i =0 ; i< Pic.arr.length;i++){
-//         let list =document.createElement('li');
-//         unorderd.appendChild(list);
-//         list.textContent =`${Pic.arr[i].name} had ${Pic.arr[i].vote} vote ,and was seen &{Pic.arr[i].name}`
-//     }
-    
+
 }
+
+
+
+function generateRandom() {
+    return Math.floor(Math.random() * Pic.arr.length);
+}
+generateRandom();
+
